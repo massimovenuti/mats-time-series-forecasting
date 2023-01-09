73b664ab53c89279845a6d4fb76a6a8cb28188e9
Almehdi Krisni <almehdi.krisni@gmail.com>
2023-01-09 03:45:46 +0100

    (Test) All dataset loading functions are working [usage is structure dependent], ILI dataset still need a stage 2 fix

--- src/main.py
+++ src/main.py
@@ -135,9 +135,7 @@ DHAT_COEF = 0.5
 
 if __name__ == "__main__":
     # stage 1
-    train_dataset, val_dataset, test_dataset = datasets.load_ld_dataset(
-        "data/ETT-TransformerTemp/ETTh1.txt"
-    )
+    train_dataset, val_dataset, test_dataset = datasets.load_weather_dataset()
 
     train_loader = DataLoader(
         train_dataset,
