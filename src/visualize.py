723d72d92e92b9c9483280c3292e43c6d8966c81
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2023-02-11 20:43:09 +0100

    added model.eval() before visualization

--- src/visualize.py
+++ src/visualize.py
@@ -8,8 +8,8 @@ from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset = "weather"
-    path = Path(config.DATA_DIR, "weather.csv")
+    dataset = "ett_m1"
+    path = Path(config.DATA_DIR, "ett_m1.txt")
     dim_h = 720
 
     # Load dataset
@@ -27,7 +27,8 @@ if __name__ == "__main__":
         config.STATES_DIR, dataset, f"mats_{dataset}_univariate_h_{dim_h}.pkl"
     )
     with save_path.open("rb") as fp:
-        model = torch.load(fp).to(device)
+        model = torch.load(fp, map_location=torch.device(device)).to(device)
+        model = model.eval()
 
     X, y = next(iter(test_loader))
     X = X[:1, :].to(device)
@@ -40,12 +41,20 @@ if __name__ == "__main__":
     Xpred = Xpred.cpu().squeeze()
 
     fig = plt.figure(figsize=(10, 3))
-    plt.plot(range(len(X)), X, label="History")
-    plt.plot(range(len(X), len(X) + dim_h), y, label="Ground truth")
-    plt.plot(range(len(X), len(X) + dim_h), Xpred[-dim_h:], label="Prediction")
+    plt.plot(range(len(X)), X, label="History", color="C0")
+    plt.plot(
+        range(len(X), len(X) + dim_h),
+        y,
+        label="Ground truth",
+        linestyle="--",
+        color="C0",
+    )
+    plt.plot(
+        range(len(X), len(X) + dim_h), Xpred[-dim_h:], label="Prediction", color="C1"
+    )
     plt.axvline(len(X), linestyle="--", color="black")
     plt.legend()
     # fig = plt.gcf()
     fig.tight_layout()
 
-    fig.savefig(Path(config.RESULTS_DIR, f"{dataset}_h_{dim_h}.pdf"))
+    fig.savefig(Path(config.RESULTS_DIR, "plots", f"{dataset}_h_{dim_h}.pdf"))
