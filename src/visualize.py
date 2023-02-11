4adc1e595b05819752f535c302ce2f5edb8cc800
Massimo VENUTI <massimo.venuti@etu.unistra.fr>
2023-02-11 20:12:08 +0100

    fixed typo

--- src/visualize.py
+++ src/visualize.py
@@ -8,9 +8,9 @@ from matplotlib import pyplot as plt
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset="weather"
+    dataset = "weather"
     path = Path(config.DATA_DIR, "weather.csv")
-    dim_h=720
+    dim_h = 720
 
     # Load dataset
     train_loader, test_loader = datasets.get_loaders(
@@ -21,24 +21,25 @@ if __name__ == "__main__":
         univariate=True,
         shuffle=False,
     )
-    
+
     # Load model
-    save_path = Path(config.STATES_DIR, dataset, f"mats_{dataset}_univariate_h_{dim_h}.pkl")
+    save_path = Path(
+        config.STATES_DIR, dataset, f"mats_{dataset}_univariate_h_{dim_h}.pkl"
+    )
     with save_path.open("rb") as fp:
         model = torch.load(fp).to(device)
-        
-        
-    X,y = next(iter(test_loader))
-    X=X[:1,:].to(device)
-    y=y[:1,:].to(device)
-    
+
+    X, y = next(iter(test_loader))
+    X = X[:1, :].to(device)
+    y = y[:1, :].to(device)
+
     Xpred = model(X, dim_h)
-    
+
     X = X.cpu().squeeze()
     y = y.cpu().squeeze()
     Xpred = Xpred.cpu().squeeze()
-    
-    fig = plt.figure(figsize=(10,3))
+
+    fig = plt.figure(figsize=(10, 3))
     plt.plot(range(len(X)), X, label="History")
     plt.plot(range(len(X), len(X) + dim_h), y, label="Ground truth")
     plt.plot(range(len(X), len(X) + dim_h), Xpred[-dim_h:], label="Prediction")
@@ -46,7 +47,5 @@ if __name__ == "__main__":
     plt.legend()
     # fig = plt.gcf()
     fig.tight_layout()
-    
-    fig.savefig(Path(config.RESULTS_DIR, f"{dataset}_h_{dim_h}.pdf")
-    
-   
+
+    fig.savefig(Path(config.RESULTS_DIR, f"{dataset}_h_{dim_h}.pdf"))
